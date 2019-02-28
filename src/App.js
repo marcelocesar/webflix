fcfb1ef97d021a10a28447915b0b5a40bcb101da
mhjesus <mhjesus@indracompany.com>
2019-02-28 18:00:41 -0300

    Comunicação rest e ajustes layout

--- src/App.js
+++ src/App.js
@@ -3,32 +3,58 @@ import logo from "./logo.svg";
 import "./assets/css/app.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: []
+    }
+  }
+
+  getMovies = async () => {
+    const LIST_MOVIES = "http://www.omdbapi.com/?s=avengers&apikey=ce58f4c8";
+    let response = await fetch(LIST_MOVIES);
+    let data = await response.json();
+    return data;
+  };
+
+  componentWillMount = async() => {
+    console.log('componentWillMount');
+    await this.getMovies().then(data => this.setState({movies: data.Search}));
+  };
+
+  listMovies = (movies) => {
+    console.log('movies', movies);
+    if (movies == null) return;
+    return movies.map((movie, idx) =>
+      <li key={idx} className="movies__itens">
+        <figure>
+          <img src={movie.Poster} alt={`Movies  movie.Title`} className="responsive-img" />
+          <figcaption>{movie.Title}</figcaption>
+        </figure>
+      </li>
     );
   }
-}
 
-class Welcome extends Component {
   render() {
-    return <h1>{this.prop.text} to React!</h1>;
+
+    const {movies} = this.state; 
+
+    return (
+      <div className="app">
+        <header className="header">
+          <div>
+            <img src={logo} className="app-logo" alt="logo" />
+            <h1>webflix</h1>
+          </div>
+        </header>
+        <main>
+          <div className="container">
+            <ul className="movies">{this.listMovies(movies)}</ul>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
